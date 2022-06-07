710ddbd81d3a5d6c60f1e74447e6df9577694032
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-07 16:56:44 -0300

    foi criado uma logica para que seja possivel mudar a mensagem caso o input apenas esteja fazio e não que o usuario não encontrado

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 
-window.addEventListener('keydown',(e) => {
-  if(e.key == 'Enter'){
+window.addEventListener('keydown', (e) => {
+  if (e.key == 'Enter') {
     userLoad()
   }
 })
 
-function userLoad(){
-  userName.innerHTML = ''
+function userLoad() {
   const usuario = document.querySelector('#userId');
+
+  if (usuario.value == '') {
+    errorMessage.innerHTML = 'digite um primeiro usuario'
+    errorMessage.classList.add('on')
+    return
+  }
+
   const url = `https://api.github.com/users/${usuario.value}`
   axios.get(url)
-  .then(response => {
-    const data = response.data
-    userName.textContent = data.name
-    userImg.src = data.avatar_url
-    userFollowers.innerHTML = `<img src="assets/followers.svg">${data.followers} Seguindo`
-    userFollowing.innerHTML = `<img src="assets/following.svg">${data.following} Seguidores`
-    userRepos.innerHTML = `<img src="assets/repository.svg">${data.public_repos} Repositórios`
-    userLocation.innerHTML = `<img src="assets/location.svg">${data.location}`
-    if(errorMessage.classList.contains('on')){
-      errorMessage.classList.remove('on')
-    }
-  })
-  .catch(error => errorMessage.classList.add('on'))
-  usuario.value=''
+    .then(response => {
+      const data = response.data
+      userName.textContent = data.name
+      userImg.src = data.avatar_url
+      userFollowers.innerHTML = `<img src="assets/followers.svg">${data.followers} Seguindo`
+      userFollowing.innerHTML = `<img src="assets/following.svg">${data.following} Seguidores`
+      userRepos.innerHTML = `<img src="assets/repository.svg">${data.public_repos} Repositórios`
+      userLocation.innerHTML = `<img src="assets/location.svg">${data.location}`
+      if (errorMessage.classList.contains('on')) {
+        errorMessage.classList.remove('on')
+      }
+    })
+    .catch(error => errorMessage.classList.add('on'))
+  usuario.value = ''
 }
